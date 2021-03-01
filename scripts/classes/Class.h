a5e123b2330a5606f8efe89f5c521e2315f57dba
aarmor01 <62306290+aarmor01@users.noreply.github.com>
2021-03-01 17:06:53 +0100

    Added basic VS project
    With SDL2, SDL2_ttf and box2d libraries (kind of organized)

---
+++ scripts/classes/Class.h
@@ -0,0 +1,8 @@
+#ifndef CLASS_H_
+#define CLASS_H_
+
+class Class
+{
+};
+
+#endif
---
+++ scripts/main.cpp
@@ -0,0 +1,26 @@
+#include "SDL.h"
+#include "SDL_ttf.h"
+#include "box2d.h"
+
+#include <iostream>
+
+using namespace std;
+
+int main(int a, char* argv[])
+{
+	//creacion de la ventana de juego
+	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_Window* window = SDL_CreateWindow("TEST", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+		900, 900, SDL_WINDOW_SHOWN);
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	//si ha habido algun problema en la creacion de la ventana de SDL, se lanza excepcion
+	if (window == nullptr || renderer == nullptr)
+		throw string("tusmuertospisaos");
+
+	b2Vec2 gravity(0.0f, -10.0f);
+
+	TTF_Init();
+
+	SDL_Delay(5000);
+	return 0;
+}
