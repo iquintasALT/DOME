e3f89abc318eb102d2c07f0a417a38fdf0225e1e
edekadt <edekadt@ucm.es>
2021-03-10 17:59:35 +0100

    Completed GameEntity, minus SpriteRenderer

--- classes/game_entity.h
+++ classes/game_entity.h
@@ -3,5 +3,15 @@
 class GameEntity :
     public GameObject
 {
+protected:
+	Vector2D velocity = Vector2D();
+	float mass = 0;
+	//TO DO
+	//SpriteRenderer* spriteRenderer
+public:
+	Vector2D getVelocity() { return velocity; };
+	void setVelocity(Vector2D newVelocity) { size = newVelocity; };
+	void scale(float scale) { size = size * scale; };
+	void update() override {};
 };
 
