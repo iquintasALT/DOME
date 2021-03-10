9b34208551d85566545dc5398ddc98a75cc0a83c
aarmor01 <62306290+aarmor01@users.noreply.github.com>
2021-03-10 16:26:45 +0100

    Added template tilemap

---
+++ assets/tilemap/Second Template Tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Second Template Tileset" tilewidth="32" tileheight="32" tilecount="24" columns="4">
+ <image source="../../resources/sprites/wardrobe_tileset.png" width="128" height="192"/>
+</tileset>
