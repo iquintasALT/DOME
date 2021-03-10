f0829e792b42de30be2f2a9097fda2fa53b67c52
edekadt <edekadt@ucm.es>
2021-03-10 17:47:33 +0100

    Completed GameObject

--- classes/game_object.h
+++ classes/game_object.h
@@ -7,11 +7,11 @@ protected:
 	Point2D position;
 	Vector2D size;
 public:
-	Point2D getPosition();
-	Vector2D getSize();
-	void setPosition();
-	void setSize();
-	void setScale();
-	void update();
+	Point2D getPosition() { return position; };
+	Vector2D getSize() { return size; };
+	void setPosition(Point2D newPosition) { position = newPosition; };
+	void setSize(Vector2D newSize) { size = newSize; };
+	void scale(float scale) {size = size * scale;};
+	void update() {};
 };
 
