6faf38ac29a1a40ca1aacb1d1991829200dfd6b2
ivasan07 <ivasan07@ucm.es>
2021-03-12 16:32:10 +0100

    Improved weapon behaviour code

--- components/weapon_behaviour.cpp
+++ components/weapon_behaviour.cpp
@@ -2,31 +2,40 @@
 
 void WeaponBehaviour::update() {
 	Vector2D playerPos = playerTr->getPos();
-	entityTr->setPos(Vector2D(playerPos.getX() + playerTr->getW() * 0.5, playerPos.getY() + playerTr->getH() * 0.15));
+	entityTr->setPos(Vector2D(playerPos.getX() + playerTr->getW() / 2, playerPos.getY() + playerTr->getH() / 2.75f - entityTr->getH() / 2));
 
 	Vector2D mousePos(ih().getMousePos().first, ih().getMousePos().second);
-	Vector2D  dir = (mousePos - entityTr->getPos()).normalize();
 
-	float angle = (atan2(dir.getY(), dir.getX()) * 180) / M_PI;
 
-	if (!flipped && (angle > 90 || angle < -90)) {
-		entityTr->setW(-entityTr->getW());
+	Vector2D yCenteredPos(entityTr->getPos().getX(), entityTr->getPos().getY() + entityTr->getH() * 0.37f); //Punto {0, Altura del cañón}  
+	Vector2D  dir = (mousePos - yCenteredPos).normalize();
+
+	float radianAngle = atan2(dir.getY(), dir.getX());
+	float degreeAngle = (radianAngle * 180) / M_PI;
+
+	if (!flipped && (degreeAngle > 90 || degreeAngle < -90)) {
+		entityImg->setFlip(SDL_FLIP_VERTICAL);
 		flipped = true;
 	}
-	else if (flipped && angle < 90 && angle > -90) {
-		entityTr->setW(-entityTr->getW());
+	else if (flipped && degreeAngle < 90 && degreeAngle > -90) {
+		entityImg->setFlip(SDL_FLIP_NONE);
 		flipped = false;
 	}
 
-	if (!flipped)
-		entityTr->setRot(angle);
-	else entityTr->setRot(angle + 180);
+	entityTr->setRot(degreeAngle);
 
 	if (ih().getMouseButtonState(InputHandler::LEFT)) {
 		Entity* bullet = entity_->getMngr()->addEntity();
 
-		float aux = abs(entityTr->getW() - 8);
-		bullet->addComponent<Transform>(entityTr->getPos() + dir * aux, dir * 6, 16, 16, angle);
+		Transform* bulletTr = bullet->addComponent<Transform>(Vector2D(), dir * 4, 16, 16, degreeAngle);
+
+		float aux1 = entityTr->getW() - bulletTr->getW() / 2; //Distancia del cañón del arma para spawnear la bala
+
+		Vector2D centeredPos = { yCenteredPos.getX() - bulletTr->getW() / 2  ,yCenteredPos.getY() - bulletTr->getH() / 2 }; //Punto para spawnear la bala centrada
+
+		bulletTr->setPos(centeredPos + dir * aux1);
+
 		bullet->addComponent<Image>(&sdlutils().images().at("player"), 2, 14, 0, 0);
 	}
+
 }
