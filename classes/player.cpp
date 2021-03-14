1358f0152a9e7c7a8a6cc0b4f82c6ccfcce0bd47
ivasan07 <ivasan07@ucm.es>
2021-03-14 18:11:11 +0100

    Fixed player error

--- classes/player.cpp
+++ classes/player.cpp
@@ -4,18 +4,24 @@ Player::Player(Manager* mngr_, Point2D pos) :GameCharacter(mngr_)
 {
 	mngr_->setHandler<Player_hdlr>(this);
 	mngr_->addEntity(this);
-	addComponent<Transform>(Point2D(), Vector2D(), 32, 64, 0);
+	addComponent<Transform>(pos, Vector2D(), 32, 64, 0);
 	addComponent<Image>(&sdlutils().images().at("player"), 2, 14, 0, 0);
 	addComponent<player_animation>();
 	addComponent<GravityComponent>();
 	addComponent<KeyboardPlayerCtrl>();
 	addComponent<InventoryController>();
-	weapon = addComponent<WeaponBehaviour>();
+
+	weapon = mngr_->addEntity();
+	Vector2D playerPos = getComponent<Transform>()->getPos();
+	Transform* playerTr = getComponent<Transform>();
+	weapon->addComponent<Transform>(Vector2D(playerPos.getX() + playerTr->getW() / 2, playerPos.getY() + playerTr->getW() * 0.4), Vector2D(), 32, 32, 0);
+	weapon->addComponent<Image>(&sdlutils().images().at("weapons"), 3, 3, 2, 2);
+	weapon->addComponent<WeaponBehaviour>(); 
 }
 
 WeaponBehaviour* Player::equipWeapon(WeaponBehaviour* newWeapon)
 {
-	WeaponBehaviour* oldWeapon = weapon;
+	WeaponBehaviour* oldWeapon = weapon->getComponent<WeaponBehaviour>();
 	*getComponent<WeaponBehaviour>() = *newWeapon;
 	return oldWeapon;
 }
--- classes/raid_scene.cpp
+++ classes/raid_scene.cpp
@@ -3,14 +3,6 @@
 void RaidScene::init() {
 
 	Player* player = new Player(mngr_, Point2D(10, 50));
-	Transform* playerTr = player->getComponent<Transform>();
-	Vector2D playerPos = playerTr->getPos();
-
-
-	Entity* weapon = mngr_->addEntity();
-	weapon->addComponent<Transform>(Vector2D(playerPos.getX() + playerTr->getW() / 2, playerPos.getY() + playerTr->getW() * 0.4), Vector2D(), 32, 32, 0);
-	weapon->addComponent<Image>(&sdlutils().images().at("weapons"), 3, 3, 2, 2);
-	weapon->addComponent<WeaponBehaviour>();
 
 	Enemy::newEnemy(mngr_, Point2D(10, 50));
 }
