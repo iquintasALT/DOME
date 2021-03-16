aa1d9c4f0e4c0835c3ec6020b2ed5c070a0aab98
ivasan07 <ivasan07@ucm.es>
2021-03-16 19:03:06 +0100

    enemy is now created with constructor + new global variables

--- game/constant_variables.h
+++ game/constant_variables.h
@@ -4,4 +4,6 @@ namespace consts {
 	const float GRAVITY = 8.0f;
 	const float FRAME_RATE = 60.0f;
 	const float PLAYER_SPEED = 3.0f;
+	const float MELEE_ENEMY_SPEED = 2.5f;
+	const float MELEE_ENEMY_STOPDISTANCE = 50.0f;
 }
