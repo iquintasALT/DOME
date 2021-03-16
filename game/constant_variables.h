1924f536c208abceb35d5ad38bec5187413daddd
Yojhan <sryojhan@gmail.com>
2021-03-16 20:02:02 +0100

    created DeltaTime

--- game/constant_variables.h
+++ game/constant_variables.h
@@ -3,6 +3,7 @@
 namespace consts {
 	const float GRAVITY = 8.0f;
 	const float FRAME_RATE = 60.0f;
+	const float DELTA_TIME = 1 / FRAME_RATE;
 	const float PLAYER_SPEED = 3.0f;
 	const float MELEE_ENEMY_SPEED = 2.5f;
 	const float MELEE_ENEMY_STOPDISTANCE = 50.0f;
