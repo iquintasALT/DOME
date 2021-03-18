59808903021a395a2c4c694dc92dac92b7cd666a
Cristian <crisca06@ucm.es>
2021-03-18 14:23:25 +0100

    Corrected minor error in animation

--- classes/shelter_scene.cpp
+++ classes/shelter_scene.cpp
@@ -3,7 +3,7 @@
 void ShelterScene::init() {
 	Entity* player = mngr_->addEntity();
 	player->addComponent<Transform>(Vector2D(), Vector2D(), 32, 64, 0);
-	player->addComponent<Image>(&sdlutils().images().at("player"), 2, 14, 0, 0);
+	player->addComponent<Image>(&sdlutils().images().at("player"), 3, 14, 0, 0);
 	player->addComponent<player_animation>();
 	player->addComponent<GravityComponent>();
 	player->addComponent<KeyboardPlayerCtrl>();
