9680799a2a79ac616aa15b78043a5d960df12cca
Yojhan <sryojhan@gmail.com>
2021-03-18 16:54:26 +0100

    Interactable elements (not tested yet)

---
+++ components/interactableElement.cpp
@@ -0,0 +1,37 @@
+#include "interactableElement.h"
+#include "../ecs/Manager.h"
+#include <iostream>
+bool InteractableElement::CheckCollision(Point2D pos) {
+	bool inCollision = false;
+
+	inCollision = pos.getX() > transform->getPos().getX() &&
+		pos.getX() < transform->getPos().getX() + transform->getW() &&
+		pos.getY() > transform->getPos().getY() &&
+		pos.getY() < transform->getPos().getY() + transform->getH();
+
+
+	if (inCollision && !isColliding) CollisionEnter();
+	if (!inCollision && isColliding) CollisionExit();
+
+	return inCollision;
+}
+
+
+void InteractableElement::CollisionEnter() {
+	std::cout << "Collision enter";
+}
+
+
+void InteractableElement::CollisionExit() {
+	std::cout << "Collision exit";
+}
+
+void InteractableElement::Interact() {
+	std::cout << "Interact OWO";
+}
+
+
+void InteractableElement::init(){
+	transform = entity_->getComponent<Transform>();
+	entity_->getMngr()->AddInteractableElement(this);
+}
