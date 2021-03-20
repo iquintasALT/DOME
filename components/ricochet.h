f93e77ab8f1a8c5c98b32df115ac23eb8c6a1593
ivasan07 <ivasan07@ucm.es>
2021-03-20 02:00:41 +0100

    Details and reestructuration of weapon classes (ricochet + classic))

--- components/ricochet.h
+++ components/ricochet.h
@@ -10,51 +10,15 @@
 
 class Ricochet : public Component {
 public:
-	Ricochet() :
-		tr_(nullptr) {
-	}
+	Ricochet(): tr_(nullptr), n(4) {}
 
-	virtual ~Ricochet() {
-	}
+	virtual ~Ricochet() {}
 
-	void init() override {
-		tr_ = entity_->getComponent<Transform>();
-		assert(tr_ != nullptr);
-	}
-	void update() override {
-		auto& pos = tr_->getPos();
-		auto& vel = tr_->getVel();
-		auto height = tr_->getH();
-		auto width = tr_->getW();
+	void init() override;
 
-		if (pos.getY() < 0) {
-			pos.setY(0);
-			vel.setY(-vel.getY());
-			n--;
-		}
-		else if (pos.getX() < 0) {
-			pos.setX(0);
-			vel.setX(-vel.getX());
-			n--;
-		}
-		else if (pos.getY() + height > sdlutils().height()) {
-			pos.setY(sdlutils().height() - height);
-			vel.setY(-vel.getY());
-			n--;
-		}
-		else if (pos.getX() + width > sdlutils().width()) {
-			pos.setX(sdlutils().width() - width);
-			vel.setX(-vel.getX());
-			n--;
-		}
-
-		if (n == 0)
-		{
-			entity_->setDead(true);
-		}
-	}
+	void update() override;
 
 private:
 	Transform* tr_;
-	int n = 4; //numero rebotes
+	int n; //numero rebotes
 };
