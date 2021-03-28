c6facf26fddd17cc182452c7b3e657478e00709d
PabloGlezAlv <62878159+PabloGlezAlv@users.noreply.github.com>
2021-03-28 19:45:15 +0200

    First tiled map

---
+++ resources/tilemap/comunication_inside.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="comunication_inside" tilewidth="16" tileheight="16" tilecount="512" columns="32">
+ <image source="../../../../P2/PROYECTOS_II/resources/sprites/sprite_tiles_32x32.png" trans="ff00ff" width="512" height="256"/>
+</tileset>
