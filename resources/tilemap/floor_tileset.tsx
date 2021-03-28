e3fc50b5e78f25896e60522a52290b1e339822d8
PabloGlezAlv <pabgon23@ucm.es>
2021-03-28 19:45:15 +0200

    First tiled map

---
+++ resources/tilemap/floor_tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="floor_tileset" tilewidth="16" tileheight="16" tilecount="112" columns="14">
+ <image source="../sprites/floor_tileset.png" trans="ff00ff" width="224" height="128"/>
+</tileset>
