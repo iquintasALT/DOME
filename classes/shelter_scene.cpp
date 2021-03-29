1357f60e7a75273422c7cee77ea32ace80458477
iquintasALT <iquintas@ucm.es>
2021-03-29 17:38:57 +0200

    gravityComponent init method created

--- classes/shelter_scene.cpp
+++ classes/shelter_scene.cpp
@@ -46,7 +46,7 @@ void ShelterScene::init() {
 	auto tr = player->addComponent<Transform>(Vector2D(), Vector2D(), 32, 64, 0);
 	player->addComponent<Image>(&sdlutils().images().at("player"), 3, 14, 0, 0);
 	player->addComponent<ParticleSystem>(&sdlutils().images().at("dust"), 1, 1, 0, 0);
-	player->addComponent<GravityComponent>(tr);
+	player->addComponent<GravityComponent>();
 	player->addComponent<KeyboardPlayerCtrl>();
 	player->addComponent<player_animation>();
 	player->addComponent<HungerComponent>();
