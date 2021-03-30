a125e59881319b6801ad33e65db7d4d020b168cc
iquintasALT <iquintas@ucm.es>
2021-03-30 18:42:19 +0200

    basic crafting widget created, but it doesnt look very good for now

--- classes/shelter_scene.h
+++ classes/shelter_scene.h
@@ -8,13 +8,14 @@
 class ShelterScene : public GameScene {
 private:
 	sdlgui::Screen* sc_;
-
+	sdlgui::ListImages craftIcons;
 public:
 	void init() override;
 	void render() override;
 	void updateScreen(SDL_Event* e);
 	void showWidget(sdlgui::Widget& widget, bool cond);
 	sdlgui::Widget& createSimpleWidget();
+	sdlgui::Widget& CraftingWidget();
 	sdlgui::Widget& createSimpleButton(sdlgui::Vector2i pos, string buttonText, string description,
 		sdlgui::Widget& ventana);
 };
