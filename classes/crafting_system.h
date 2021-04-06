8a461f6599d92e76b1918954afe58a34675b0e35
ivasan07 <ivasan07@ucm.es>
2021-04-06 18:42:52 +0200

    progress with rendering the craft menu

--- classes/crafting_system.h
+++ classes/crafting_system.h
@@ -17,10 +17,14 @@ enum ITEMS {
 struct ITEMS_SPRITESHEET_INFO {
 	int row;
 	int col;
+	std::string itemName;
 };
 
 const vector<ITEMS_SPRITESHEET_INFO>ITEMS_INFO = {
-	//poner fila y columna de cada item
+	{0,0,"bandage"},{0,1,"medical components"},{0,2,"water"},{1,0,"organic material"},{1,1,"mecanical components"},
+	{1,2,"antidote"},{2,0,"food"},{0,0,"splint"},{0,1,"spaceship rockets"},{0,2,"spaceship key items"},{1,0,"building parts"},
+	{1,1,"electronic remains"},{1,2,"metal plates"},{2,0,"weapon upgrade"},{0,0,"upgrade kit"},{0,1,"clasic ammo"},{0,2,"backpack upgrade"},
+	{1,0,"armour upgrade"}
 };
 
 struct CraftableItem {
