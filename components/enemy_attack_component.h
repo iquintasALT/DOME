5e130d28278d9f79180b0997ec8606a57be51cb0
edekadt <edekadt@ucm.es>
2021-04-07 13:50:26 +0200

    Added generic melee attack, works for aerial enemies

--- components/enemy_attack_component.h
+++ components/enemy_attack_component.h
@@ -10,6 +10,8 @@ protected:
     Transform* tr_;
     Transform* playerTr_;
 
+    EnemyAttackComponent(Transform* transform = nullptr, Transform* playerTransform = nullptr) : 
+        tr_(transform), playerTr_(playerTransform) {};
     // attack is bool so that behaviour knows whether to start attack cooldown
     virtual bool attack() = 0;
     // Returns a vector pointing towards the player
@@ -21,15 +23,27 @@ public:
 
 //--------------------------------------------------------------------------------------------------------------
 
-class GroundedMeleeAttack :
+class MeleeAttack :
     public EnemyAttackComponent
 {
 protected:
-    // When the enemy performs its attack, it will lunge at the player with this boost of speed
-    Vector2D lunge = Vector2D();
-    // Distance from which the enemy can attack
-    float reach;
+    /// When the enemy performs its attack, it will lunge at the player with this boost of speed
+    /// Both values should be positive, and only represent magnitude
+    Vector2D lunge;
 
     /// Leaps at player, entering attack mode
-    virtual bool attack();
+    virtual bool attack() override;
+public:
+    MeleeAttack(Vector2D lungeThrust = Vector2D(3.0, 2.0)) : lunge(lungeThrust) {};
+};
+
+//--------------------------------------------------------------------------------------------------------------
+
+class GroundedMeleeAttack :
+    public MeleeAttack
+{
+protected:
+    virtual bool attack() override;
+public:
+    GroundedMeleeAttack(Vector2D lungeThrust = Vector2D(3.0, 2.0)) : MeleeAttack(lungeThrust) {};
 };
