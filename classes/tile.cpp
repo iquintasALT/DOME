1f0012e396c566a9539423401f0972e7887f1106
AaronNauzetMorenoSosa <aarmor01@ucm.es>
2021-04-11 15:33:00 +0200

    Optimised tile renderering so now it does not depend on layer order, but on the property renderLayer

--- classes/tile.cpp
+++ classes/tile.cpp
@@ -1,8 +1,12 @@
 #include "tile.h"
 #include "../ecs/Entity.h"
 
-Tile::Tile(Manager* mngr, Texture* tset, int x, int y, int tx, int ty, int w, int h) {
+Tile::Tile(Manager* mngr, Texture* tset, string layer, int x, int y, int tx, int ty, int w, int h) {
 	auto tile = mngr->addEntity();
 	Component* tr = tile->addComponent<TileRenderer>(tset, x, y, tx, ty, w, h);
-	mngr->addRenderLayer<Background>(tr);
+
+	if(layer == string("Background"))
+		mngr->addRenderLayer<Background>(tr);
+	else 
+		mngr->addRenderLayer<Walls>(tr);
 }
