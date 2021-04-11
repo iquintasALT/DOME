5c6a5d5ce205363821372ce1c865469b07930fb5
ivasan07 <ivasan07@ucm.es>
2021-04-11 13:28:16 +0200

    little update

--- classes/charge_weapon.h
+++ classes/charge_weapon.h
@@ -9,7 +9,10 @@ private:
 	int charger = 30; //Pasar por referencia cuando este
 	int actcharger = 30;
 	int nbullets = 50;
+
 public:
+
+	int getChargerBullets() { return actcharger; }
 	ChargeWeapon(float fR, int dam);
 	virtual void update();
 };
