2f50b577051ca7e81a81f22d3364195a2f73775c
Cristian <crisca06@ucm.es>
2021-04-19 15:52:58 +0200

    Added animation to flying enemy and adjusted player animations

--- components/enemy_animation.cpp
+++ components/enemy_animation.cpp
@@ -49,3 +49,43 @@ bool enemy_animation::changeAnimations() {
 	}
 	return false;
 }
+
+//-----------------------------------------------------------------------------------------------------------
+
+flying_enemy_animation::flying_enemy_animation() : tr_(nullptr), im_(nullptr), rb(nullptr) {};
+
+flying_enemy_animation::~flying_enemy_animation() {}
+
+void flying_enemy_animation::init() {
+	currentAnimation.setImage(im_ = entity_->getComponent<Image>());
+
+	for (Animation& anim : animations) {
+		anim.setImage(im_);
+	}
+
+	tr_ = entity_->getComponent<Transform>();
+	rb = entity_->getComponent<RigidBody>();
+	assert(tr_ != nullptr && im_ != nullptr && rb != nullptr);
+}
+
+void flying_enemy_animation::update() {
+	if (changeAnimations()) {
+		timer = 0; return;
+	}
+	timer += currentAnimation.animationSpeed();
+
+	if (timer > 1) {
+		currentAnimation.advanceFrame();
+		timer = 0;
+	}
+}
+
+bool flying_enemy_animation::changeAnimations() {
+
+	float x = rb->getVel().getX();
+
+	if (x < 0) im_->setFlip(SDL_FLIP_HORIZONTAL);
+	else  im_->setFlip(SDL_FLIP_NONE);
+
+	return false;
+}
