76e0b46200ca7af508717bb97248d5aaa94e3c8e
ivasan07 <ivasan07@ucm.es>
2021-04-25 18:55:44 +0200

    fixed render of menus in shelter scene

---
+++ components/open_station.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "../ecs/Entity.h"
+#include "../ecs/Component.h"
+#include "../components/Transform.h"
+#include "../sdlutils/InputHandler.h"
+#include "../ecs/Manager.h"
+#include "../classes/workshop.h"
+#include "../classes/sleep_station.h"
+
+class Open_station: public Component
+{
+private:
+	Transform* myTr;
+	Transform* playerTr;
+	Entity* station;
+
+	bool eClicked;
+
+public:
+	Open_station(Entity* st);
+	void init();
+	void update();
+};
+
