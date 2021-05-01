7f8cc1754fafec6c0b7418fbe7da4a144e372331
iquintasALT <iquintas@ucm.es>
2021-05-01 13:41:55 +0200

    nanogui methods removed
    nanogui classes haven't been removed yet

--- classes/shelter_scene.h
+++ classes/shelter_scene.h
@@ -10,7 +10,6 @@
 
 class ShelterScene : public GameScene {
 private:
-	sdlgui::Screen* sc_;
 	sdlgui::ListImages craftIcons;
 	CraftingSystem* craftSys;
 	Workshop* mechanical_Workshop;
@@ -24,8 +23,4 @@ public:
 	void init() override;
 	virtual void update();
 	virtual void render() override;
-	void updateScreen(SDL_Event* e);
-	void showWidget(sdlgui::Widget& widget, bool cond);
-	sdlgui::Widget& createSimpleWidget();
-	sdlgui::Widget& createSimpleButton(sdlgui::Vector2i pos, string buttonText, string description, std::function<void()> func);
 };
