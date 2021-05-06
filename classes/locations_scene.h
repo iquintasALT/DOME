12e8f9a6c9004424686dc0eda4e70bbc27eea650
PabloFernandezAl01 <pablfe20@ucm.es>
2021-05-06 19:17:29 +0200

    ordered location buttons

--- classes/locations_scene.h
+++ classes/locations_scene.h
@@ -5,11 +5,12 @@
 #include "../game/constant_variables.h"
 
 class Game;
+class Texture;
 
-const array<string, consts::NUM_LOCATIONS> paths = { "./resources/tilemap/zona_supermarket.tmx", "./resources/tilemap/zona_shop.tmx",
-												"./resources/tilemap/zona_hospital.tmx", "./resources/tilemap/zona_comunications.tmx", 
-																					"./resources/tilemap/zona_nuclear_power_station.tmx" };
-const array<string, consts::NUM_LOCATIONS> names = {"SUPERMARKET", "SHOP", "HOSPITAL", "COMNUNICATIONS", "NUCLEAR POWER STATION"};
+const array<string, consts::NUM_LOCATIONS> paths = { "./resources/tilemap/zona_supermarket.tmx", "./resources/tilemap/zona_comunications.tmx",
+												"./resources/tilemap/zona_hospital.tmx", "./resources/tilemap/zona_nuclear_power_station.tmx", 
+																					"./resources/tilemap/zona_shop.tmx" };
+const array<string, consts::NUM_LOCATIONS> names = {"SUPERMARKET", "COMNUNICATIONS", "HOSPITAL", "NUCLEAR POWER STATION", "SHOP"};
 
 class LocationsScene : public GameScene
 {
@@ -25,6 +26,7 @@ public:
 	LocationsScene(Game* g) : GameScene(g, "Locations") {};
 	void init() override;
 	void update() override;
+	void addInfoText(Texture* t, Vector2D pos, int xSize, int ySize);
 
 	void changeToRaid(Game* g, int index);
 	void anActualGoodName(Game* g);
