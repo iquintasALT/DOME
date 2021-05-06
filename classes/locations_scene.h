60fb9438700b3e6f37a41b9e8648d4be588ca3e6
PabloFernandezAl01 <pablfe20@ucm.es>
2021-05-07 00:07:06 +0200

    starting nuclear power station tilemap

--- classes/locations_scene.h
+++ classes/locations_scene.h
@@ -7,10 +7,10 @@
 class Game;
 class Texture;
 
-const array<string, consts::NUM_LOCATIONS> paths = { "./resources/tilemap/zona_supermarket.tmx", "./resources/tilemap/zona_comunications.tmx",
-												"./resources/tilemap/zona_hospital.tmx", "./resources/tilemap/zona_nuclear_power_station.tmx", 
+const array<string, consts::NUM_LOCATIONS> paths = { "./resources/tilemap/zona_supermarket.tmx", "./resources/tilemap/zona_nuclear_power_station.tmx", 
+												"./resources/tilemap/zona_hospital.tmx", "./resources/tilemap/zona_comunications.tmx", 
 																					"./resources/tilemap/zona_shop.tmx" };
-const array<string, consts::NUM_LOCATIONS> names = {"SUPERMARKET", "COMNUNICATIONS", "HOSPITAL", "NUCLEAR POWER STATION", "SHOP"};
+const array<string, consts::NUM_LOCATIONS> names = {"SUPERMARKET", " NUCLEAR POWER STATION", "HOSPITAL", "COMNUNICATIONS", "SHOP"};
 
 class LocationsScene : public GameScene
 {
