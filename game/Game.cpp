2556f6363170ea27ce89757c9b9c4d25650529bf
ivasan07 <ivasan07@ucm.es>
2021-05-09 21:09:24 +0200

    fixed the damn annoying back to menu error

--- game/Game.cpp
+++ game/Game.cpp
@@ -83,8 +83,15 @@ void Game::start() {
 }
 
 void Game::initLoot() {
-	// ITEMS n, int cantidad (INUTIL PARA ESTE METODO), int w , int h , int x, int y,int row, int col,string desc
-	SCENES_LOOT.emplace(SCENES::RAID, vector<vector<I>>{ {I(WATER, 3, 1, 2, 0, 0,4,0,"pues eso, agua. Se bebe"), I(MEDICAL_COMPONENTS, 5, 2, 2, 1, 0,3,2,"componentes medicos para ponerte hasta el culo")} });
+	//, HOSPITAL, RESTAURANT, RAID, COMMUNICATIONS,NUCLEAR_STATION,SUPERMARKET,SHOP
+// ITEMS n, int cantidad (INUTIL PARA ESTE METODO), int w , int h , int x, int y,int row, int col,string desc
+	SCENES_LOOT.emplace(SCENES::RAID, vector<vector<I>>{ {I(WATER, 3, 1, 2, 0, 0, 4, 0, "pues eso, agua. Se bebe"), I(MEDICAL_COMPONENTS, 5, 2, 2, 1, 0, 3, 2, "componentes medicos para ponerte hasta el culo")} });
+	SCENES_LOOT.emplace(SCENES::RESTAURANT, vector<vector<I>>{});
+	SCENES_LOOT.emplace(SCENES::HOSPITAL, vector<vector<I>>{});
+	SCENES_LOOT.emplace(SCENES::COMMUNICATIONS, vector<vector<I>>{});
+	SCENES_LOOT.emplace(SCENES::NUCLEAR_STATION, vector<vector<I>>{});
+	SCENES_LOOT.emplace(SCENES::SUPERMARKET, vector<vector<I>>{});
+	SCENES_LOOT.emplace(SCENES::SHOP, vector<vector<I>>{});
 }
 
 
