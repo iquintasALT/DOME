3b8731523ee486dd1ab4662747b424d2396e5919
edekadt <edekadt@ucm.es>
2021-05-15 23:15:54 +0200

    Changed all weapons to fit into the same position in component array

--- classes/weapon_behaviour.h
+++ classes/weapon_behaviour.h
@@ -1,22 +1,11 @@
 #pragma once
-//#include "../ecs/Component.h"
-//#include "../ecs/Entity.h"
-//#include "../components/Transform.h"
-//#include "../game/constant_variables.h"
-//#include "../ecs/Manager.h"
-//#include "../sdlutils/InputHandler.h"
-//#include "../components/Image.h"
-//#include "../sdlutils/SDLUtils.h"
 #include "../classes/game_entity.h"
 #include "../game/ecs_defs.h"
-//#include "../ecs/Manager.h"
 #include "../components/weapon.h"
 #include "../utils/checkML.h"
 #include "../classes/ricochet_weapon.h"
 #include "../classes/charge_weapon.h"
 #include <math.h>
-
-enum class WeaponType { CLASSIC, RICOCHET, LASER};
 
 class WeaponAnimation;
 class WeaponBehaviour : public GameEntity {
@@ -24,15 +13,14 @@ private:
 	Weapon* weapon;
 	Transform* pl;
 	WeaponAnimation* animator_;
-	WeaponType weaponType_;
 
-	short int weaponTiers [3] ={0, 0, 0};
+	Weapon::WeaponType weaponType;
+	short int weaponTiers [3] = {0, 0, 0};
 
 	InventoryController* inv;
 
 	int weaponBullets[3] = { 0, 0, 0 };
 	int bulletSpread = 0;
-	inline short int weaponType() { return (short int)weaponType_; }; // Quick conversion from enum to array index
 
 public:
 	WeaponBehaviour(Manager* mngr, Vector2D playerPos, Transform* playerTr);
@@ -45,7 +33,7 @@ public:
 
 	void changeWeapon();
 
-	WeaponType typeOfWeapon() { return weaponType_; }
+	Weapon::WeaponType typeOfWeapon() { return weaponType; }
 	int tierOfWeapon();
 	void upgradeCurrentWeapon();
 
