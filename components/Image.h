be529c9973587de4b0935602dc3bdd6660983992
aarmor01 <62306290+aarmor01@users.noreply.github.com>
2021-05-15 19:13:05 +0200

    Added a theoretical way to put an alpha to an specific image, not to the texture itself

--- components/Image.h
+++ components/Image.h
@@ -13,14 +13,14 @@
 
 class Image : public Component {
 public:
-	Image(Texture* tex, bool isUI_ = false) : tr_(nullptr), scrollFactor(1) ,tex_(tex), src_({ 0,0,tex->width(),tex->width() }),
-		flip_(SDL_FLIP_NONE), rotationOrigin({ -1, -1 }), isUI(isUI_) {}
+	Image(Texture* tex, bool isUI_ = false) : tr_(nullptr), scrollFactor(1), tex_(tex), src_({ 0,0,tex->width(),tex->width() }),
+		flip_(SDL_FLIP_NONE), rotationOrigin({ -1, -1 }), isUI(isUI_), alpha_(255) {}
 
 	Image(Texture* tex, SDL_Rect src, bool isUI_ = false) : tr_(nullptr), tex_(tex), scrollFactor(1), src_(src),
-		flip_(SDL_FLIP_NONE), rotationOrigin({ -1, -1 }), isUI(isUI_) {}
+		flip_(SDL_FLIP_NONE), rotationOrigin({ -1, -1 }), isUI(isUI_), alpha_(255) {}
 
-	Image(Texture* tex, int rows, int cols, int r, int c, bool isUI_ = false) : tr_(nullptr), tex_(tex), scrollFactor(1), flip_(SDL_FLIP_NONE),
-		rotationOrigin({ -1, -1 }), isUI(isUI_)
+	Image(Texture* tex, int rows, int cols, int r, int c, bool isUI_ = false) : tr_(nullptr), tex_(tex), scrollFactor(1),
+		flip_(SDL_FLIP_NONE), rotationOrigin({ -1, -1 }), isUI(isUI_), alpha_(255)
 	{
 		int w = tex->width() / cols;
 		int h = tex->height() / rows;
@@ -52,10 +52,18 @@ public:
 			dest.h *= scale;
 		}
 
+		// en caso de que esta imagen tenga algun tipo de alpha menor a 255
+		if (alpha_ < 255)
+			tex_->setAlpha(alpha_);
+
 		if (rotationOrigin.x == -1 && rotationOrigin.y == -1)
 			tex_->render(src_, dest, tr_->getRot(), nullptr, flip_);
 		else
 			tex_->render(src_, dest, tr_->getRot(), &rotationOrigin, flip_);
+
+		// en caso de que esta imagen tenga algun tipo de alpha menor a 255
+		if (alpha_ < 255)
+			tex_->setAlpha(255);
 	}
 
 	// util por si queremos hacer algun tipo de pequeña animacion en cosas como tooltips o interfaces
@@ -88,7 +96,7 @@ public:
 	}
 
 	void setAlpha(Uint8 alpha) {
-		tex_->setAlpha(alpha);
+		alpha_ = alpha;
 	}
 
 	//Value between 0-1
@@ -103,13 +111,16 @@ public:
 	}
 
 private:
-	bool isUI;
 	Transform* tr_;
 	Texture* tex_;
-	SDL_Rect src_;
+
+	bool isUI;
 	float scrollFactor;
-	SDL_RendererFlip flip_;
+	std::size_t alpha_ = 255;
+
+	SDL_Rect src_;
 	SDL_Point rotationOrigin;
+	SDL_RendererFlip flip_;
 };
 
 
