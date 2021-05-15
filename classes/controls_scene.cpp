8d033b20b0ae00f5c4f097db25ccc9d23ec0f37c
Cristian <crisca06@ucm.es>
2021-05-15 22:12:37 +0200

    Improved controls scene and added scrolling background to controls scene

--- classes/controls_scene.cpp
+++ classes/controls_scene.cpp
@@ -4,11 +4,27 @@
 #include "../sdlutils/SDLUtils.h"
 #include "../game/constant_variables.h"
 #include "locations_scene.h"
+#include "menu_scene.h"
 
 void ControlsScene::init()
 {
-	auto playButton = new PauseButton(Vector2D(0, 0), Vector2D(consts::WINDOW_WIDTH,consts::WINDOW_HEIGHT), &sdlutils().images().at("controls"), goGame, g_, mngr_, 0, " ");
+	float size = 1.2f;
+	auto a = mngr_->addEntity();
+	std::vector<Texture*> textures(5);
+	for (int i = 2; i < 7; i++) {
+		std::string str = "bgImage" + std::to_string(i);
+		textures[i-2] = &sdlutils().images().at(str);
+	}
+	a->addComponent<ScrollingBackGround>(consts::WINDOW_WIDTH * size, consts::WINDOW_HEIGHT * size, textures, .2, true);
+	mngr_->addRenderLayer<Interface>(a);
+
+	auto playButton = new PauseButton(Vector2D(0, 0), Vector2D(consts::WINDOW_WIDTH, consts::WINDOW_HEIGHT), &sdlutils().images().at("black"), goGame, g_, mngr_, 0, " ");
 	mngr_->addEntity(playButton);
+
+	auto controls = mngr_->addEntity();
+	controls->addComponent<Transform>(Vector2D(0.0, 0.0))->setSize(sdlutils().width(), sdlutils().height());
+	controls->addComponent<Image>(&sdlutils().images().at("controls"), 1, 3, 0, 1);
+	mngr_->addRenderLayer<ULTIMATE>(controls);
 }	
 
 
