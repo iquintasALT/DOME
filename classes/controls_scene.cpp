f8969d664c74add5482238397d7098469345b12f
PabloFernandezAl01 <pablfe20@ucm.es>
2021-05-16 12:24:29 +0200

    remove the button from control scene

--- classes/controls_scene.cpp
+++ classes/controls_scene.cpp
@@ -6,20 +6,11 @@
 #include "locations_scene.h"
 #include "menu_scene.h"
 
-void ControlsScene::init()
-{
-	float size = 1.2f;
-	auto a = mngr_->addEntity();
-	std::vector<Texture*> textures(5);
-	for (int i = 2; i < 7; i++) {
-		std::string str = "bgImage" + std::to_string(i);
-		textures[i-2] = &sdlutils().images().at(str);
-	}
-	a->addComponent<ScrollingBackGround>(consts::WINDOW_WIDTH * size, consts::WINDOW_HEIGHT * size, textures, .2, true);
-	mngr_->addRenderLayer<Interface>(a);
-
-	auto playButton = new PauseButton(Vector2D(0, 0), Vector2D(consts::WINDOW_WIDTH, consts::WINDOW_HEIGHT), &sdlutils().images().at("black"), goGame, g_, mngr_, 0, " ");
-	mngr_->addEntity(playButton);
+void ControlsScene::init() {
+	auto background = mngr_->addEntity();
+	background->addComponent<Transform>(Vector2D(0.0, 0.0), consts::WINDOW_WIDTH, consts::WINDOW_HEIGHT);
+	background->addComponent<Image>(&sdlutils().images().at("bgImage"), true);
+	mngr_->addRenderLayer<ULTIMATE>(background);
 
 	auto controls = mngr_->addEntity();
 	controls->addComponent<Transform>(Vector2D(0.0, 0.0))->setSize(sdlutils().width(), sdlutils().height());
@@ -27,15 +18,9 @@ void ControlsScene::init()
 	mngr_->addRenderLayer<ULTIMATE>(controls);
 }	
 
-
-void ControlsScene::goGame(Manager* mngr) {
-	ih().clearState();
-	mngr->ChangeScene(new LocationsScene(mngr->getGame()), SceneManager::SceneMode::ADDITIVE);
-}
-
 void ControlsScene::update() {
 	GameScene::update();
-	if (ih().keyDownEvent()) {
+	if (ih().mouseButtonEvent() || ih().keyDownEvent()) {
 		ih().clearState();
 		mngr_->ChangeScene(new LocationsScene(mngr_->getGame()), SceneManager::SceneMode::ADDITIVE);
 	}
