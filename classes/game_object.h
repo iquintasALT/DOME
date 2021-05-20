e3a85b748ca379d4ff6d1f6c478552d782ef15bb
aarmor01 <62306290+aarmor01@users.noreply.github.com>
2021-05-20 21:31:21 +0200

    It was all just an elaborated prank from the GO file :pensive:

---
+++ classes/game_object.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "../ecs/Entity.h"
+#include "../utils/Vector2D.h"
+#include "../utils/checkML.h"
+
+class GameObject : public Entity
+{
+protected:
+	Point2D position = Point2D();
+	Vector2D size = Vector2D();
+
+public:
+	inline GameObject(Manager* mngr_) : Entity(mngr_) {}
+	inline Point2D getPosition() { return position; };
+	inline Vector2D getSize() { return size; };
+	inline void setPosition(Point2D newPosition) { position = newPosition; };
+	inline void setSize(Vector2D newSize) { size = newSize; };
+	inline void scale(float scale) { size = size * scale; };
+	//virtual void update() {};
+};
+
