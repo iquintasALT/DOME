3591ca134ea4ffed71c322b997564d4cd9d41fb9
Yojhan <sryojhan@gmail.com>
2021-05-20 15:36:14 +0200

    intial scene

--- classes/InitialScene.cpp
+++ classes/InitialScene.cpp
@@ -2,7 +2,8 @@
 #include "../components/CameraMovement.h"
 #include "../components/CameraShake.h"
 #include "../components/Dialogue.h"
-
+#include "../components/TransitionComponent.h"
+#include "../components/Timer.h"
 #include <iostream>
 void InitialScene::init()
 {
@@ -11,11 +12,33 @@ void InitialScene::init()
 
 	Camera::mainCamera->MoveToPoint(mngr_->getHandler<Player_hdlr>()->getComponent<Transform>()->getPos());
 	mngr_->getHandler<Player_hdlr>()->getComponent<CameraMovement>()->enabled = false;
-	auto a = mngr_->addEntity();
-	a->addComponent<CameraShake>(10, 15, 3, [this]() {
+	mngr_->getHandler<Player_hdlr>()->getComponent<KeyboardPlayerCtrl>()->enabled = false;
+
+
+	auto startScene = [this]() {
 		auto a = mngr_->addEntity();
-		a->addComponent<Transform>(Vector2D(0,200), 300, 400);
-		auto d = a->addComponent<Dialogue>();
-		d->createText("QUE HA SIDO ESO???", 20);
-		});
+		auto t = mngr_->addEntity();
+		t->addComponent<Transform>(Vector2D(), consts::WINDOW_WIDTH, consts::WINDOW_HEIGHT);
+		t->addComponent<Image>(&sdlutils().images().at("black"), true);
+		t->addComponent<TransitionComponent>(2);
+		mngr_->addRenderLayer<ULTIMATE>(t);
+
+		a->addComponent<CameraShake>(10, 15, 3, [this]() {
+	
+			auto a = mngr_->addEntity();
+			a->addComponent<Transform>(Vector2D(0, 200), 300, 400);
+			auto d = a->addComponent<Dialogue>();
+			std::vector<std::string> texts = { "QUE HA SIDO ESO???", "Hmmm tendre que darme prisa" };
+			d->createText(texts, 20);
+			});
+	};
+
+	auto black = mngr_->addEntity();
+	black->addComponent<Transform>(Vector2D(), consts::WINDOW_WIDTH, consts::WINDOW_HEIGHT);
+	auto texture = &sdlutils().images().at("black");
+	texture->setAlpha(255);
+	auto img = black->addComponent<Image>(texture, true);
+	black->addComponent<Timer>(5, startScene);
+	mngr_->addRenderLayer<ULTIMATE>(black);
+
 }
--- components/Dialogue.cpp
+++ components/Dialogue.cpp
@@ -11,45 +11,81 @@
 #include "KeyboardPlayerCtrl.h"
 #include "Timer.h"
 
-void Dialogue::init()
+Dialogue::Dialogue(int height):
+	height(height)
 {
 	text = nullptr;
+	index = -1;
+
+	textSpeed = 1;
+
+	function = []() {};
 }
+
+
 
 void Dialogue::update()
 {
 	if (text != nullptr && text->finishedWriting) {
 		if (ih().isKeyDown(SDLK_SPACE)) {
 			text->getEntity()->setDead(true);
-			background->setDead(true);
-
 			text = nullptr;
-			background = nullptr;
-			auto timer = entity_->getMngr()->addEntity();
-			timer->addComponent<Timer>(0.5f, [this]() {
-				Entity* ent = entity_->getMngr()->getHandler<Player_hdlr>();
-				ent->getComponent<KeyboardPlayerCtrl>()->enabled = true;
-				entity_->setDead(true);
-				});
+
+			if (++index > 0 && index < texts.size()) { //Continue
+				showText(texts[index]);
+			}
+			else { //It is finished
+				background->setDead(true);
+
+				text = nullptr;
+				background = nullptr;
+				auto timer = entity_->getMngr()->addEntity();
+
+				function();
+
+				timer->addComponent<Timer>(0.5f, [this]() {
+					Entity* ent = entity_->getMngr()->getHandler<Player_hdlr>();
+					ent->getComponent<KeyboardPlayerCtrl>()->enabled = true;
+					entity_->setDead(true);
+					});
+			}
 		}
 	}
 }
 
 void Dialogue::createText(std::string txt, float speed)
 {
+	textSpeed = speed;
+
+	index = -1;
+
+	showBackground();
+	showText(txt);
+
+	Entity* player = entity_->getMngr()->getHandler<Player_hdlr>();
+	player->getComponent<KeyboardPlayerCtrl>()->enabled = false;
+}
+
+void Dialogue::createText(std::vector<std::string> texts, float speed)
+{
+	this->texts = texts;
+	createText(texts[0], speed);
+	index = 0;
+}
+
+void Dialogue::showBackground() {
 	background = entity_->getMngr()->addEntity();
 	entity_->getMngr()->addRenderLayer<Interface>(background);
 	background->addComponent<Transform>(
 		Vector2D(0, consts::WINDOW_HEIGHT - 200), consts::WINDOW_WIDTH, 200);
-	auto a = background->addComponent<Image>(&sdlutils().images().at("panel"), true);
+	background->addComponent<Image>(&sdlutils().images().at("panel"), true);
+}
 
+void Dialogue::showText(std::string txt)
+{
 	Entity* ent = entity_->getMngr()->addEntity();
 	entity_->getMngr()->addRenderLayer<Interface>(ent);
 	ent->addComponent<Transform>(
 		Vector2D(50, consts::WINDOW_HEIGHT - 150), consts::WINDOW_WIDTH - 50, 200);
-	text = ent->addComponent<TextWithBackground>(txt, sdlutils().fonts().at("ARIAL24"), build_sdlcolor(0xfffffffff), nullptr, true, speed, false);
-
-	Entity* player = entity_->getMngr()->getHandler<Player_hdlr>();
-	player->getComponent<KeyboardPlayerCtrl>()->enabled = false;
-
+	text = ent->addComponent<TextWithBackground>(txt, sdlutils().fonts().at("ARIAL24"), build_sdlcolor(0xfffffffff), nullptr, true, textSpeed, false);
 }
