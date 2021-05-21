b9f40a2985eb6b4063e54fe97e6e9dd9c42d7a40
Yojhan <sryojhan@gmail.com>
2021-05-22 01:02:03 +0200

    NO MORE PLAYER COLLISIONS :angel:

--- components/Transform.cpp
+++ components/Transform.cpp
@@ -1,9 +1,9 @@
 #include "Transform.h"
 
 Transform::Transform() :
-	pos_(), width_(), height_(), rotation_(), collisions_(nullptr) { }
+	pos_(), width_(), height_(), rotation_(){ }
 
 Transform::Transform(Vector2D pos, float width, float height,
-	float rotation) : pos_(pos), width_(width), height_(height), rotation_(rotation), collisions_(nullptr) { }
+	float rotation) : pos_(pos), width_(width), height_(height), rotation_(rotation) { }
 
 Transform:: ~Transform() { }
