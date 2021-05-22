b29c45d84d8e3ebf2cb4b1ff18d8fd2fdd8e6a1c
ivasan07 <ivasan07@ucm.es>
2021-05-22 16:07:24 +0200

    weapon upgrade icon

--- classes/crafting_system.h
+++ classes/crafting_system.h
@@ -29,7 +29,7 @@ struct ITEMS_SPRITESHEET_INFO {
 const vector<ITEMS_SPRITESHEET_INFO>ITEMS_INFO = {
 	{0,2,"bandage"},{0,1,"medical components"},{4,0,"water"},{1,2,"organic material"},{4,1,"mecanical components"},
 	{1,0,"antidote"},{2,0,"food"},{5,1,"splint"},{6,0,"spaceship rockets"},{4,2,"spaceship key items"},{7,0,"building parts"},
-	{5,0,"electronic remains"},{7,1,"metal plates"},{2,0,"weapon upgrade"},{6,2,"upgrade kit"},{2,1,"clasic ammo"},{3,1,"ricochet ammo"},{3,0,"laser ammo"},
+	{5,0,"electronic remains"},{7,1,"metal plates"},{7,2,"weapon upgrade"},{6,2,"upgrade kit"},{2,1,"clasic ammo"},{3,1,"ricochet ammo"},{3,0,"laser ammo"},
 	{5,2,"spaceship radar"}, {6,1,"spaceship cabin"}, {0,0,"painkiller"}
 };
 
