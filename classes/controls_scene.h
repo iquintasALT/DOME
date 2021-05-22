8d2e71f53c4a7f0692e2ea121db5e733d8eedf7c
iquintasALT <iquintas@ucm.es>
2021-05-22 20:43:36 +0200

    initial tutorial for qa

--- classes/controls_scene.h
+++ classes/controls_scene.h
@@ -5,6 +5,11 @@
 
 class ControlsScene : public GameScene
 {
+private:
+	string bgImage = "bgImage";
+	string images[4] = { "controls", "locationsTutorial", "raidTutorial", "shelterTutorial" };
+	int cont = 1;
+	Entity* controls;
 public:
 	ControlsScene(Game* game) : GameScene(game, "Controles") {};
 	void init() override;
