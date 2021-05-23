b6c80925fcabef7ddcff7c99deb6d26578d21c33
iquintasALT <iquintas@ucm.es>
2021-05-23 19:07:33 +0200

    Update Inventory.cpp

--- components/Inventory.cpp
+++ components/Inventory.cpp
@@ -349,7 +349,7 @@ void Inventory::storeDefaultItems() {
 
 	storeItem(new Item(ItemInfo::food(), entity_->getMngr(), this, 2, 0));
 	storeItem(new Item(ItemInfo::food(), entity_->getMngr(), this, 3, 0));
-	storeItem(new Item(ItemInfo::water(), entity_->getMngr(), this, 4, 0, 30));
+	storeItem(new Item(ItemInfo::water(), entity_->getMngr(), this, 4, 0));
 	//balas
 	storeItem(new Item(ItemInfo::classicAmmo(), entity_->getMngr(), this, 0, 2, 13));
 	storeItem(new Item(ItemInfo::laserAmmo(), entity_->getMngr(), this, 0, 3, 4));
@@ -359,6 +359,8 @@ void Inventory::storeDefaultItems() {
 	storeItem(new Item(ItemInfo::bandage(), entity_->getMngr(), this, 8, 0, 0));
 	storeItem(new Item(ItemInfo::antidote(), entity_->getMngr(), this, 8, 1));
 	storeItem(new Item(ItemInfo::splint(), entity_->getMngr(), this, 8, 3));
+	//
+	storeItem(new Item(ItemInfo::medicalComponents(), entity_->getMngr(), this, 6, 3));
 }
 
 
