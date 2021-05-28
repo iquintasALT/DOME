c27dd88ab4605fbe21ce5c32adda41890c86b7d4
PabloFernandezAl01 <pablfe20@ucm.es>
2021-05-28 21:43:54 +0200

    Update Game.cpp

--- game/Game.cpp
+++ game/Game.cpp
@@ -150,7 +150,7 @@ void Game::initLoot() {
 		},
 		{
 			make_pair(CraftingSystem::getItemInfo(UPGRADE_KIT), Vector2D(0, 0)), make_pair(CraftingSystem::getItemInfo(FOOD), Vector2D(2, 2)),
-			make_pair(CraftingSystem::getItemInfo(FOOD), Vector2D(3, 1)),
+			make_pair(CraftingSystem::getItemInfo(METAL_PLATES), Vector2D(3, 1)),
 			make_pair(CraftingSystem::getItemInfo(ORGANIC_MATERIAL), Vector2D(2, 3)), make_pair(CraftingSystem::getItemInfo(LASER_AMMO), Vector2D(4, 0)),
 			make_pair(CraftingSystem::getItemInfo(BUILDING_PARTS), Vector2D(0, 2)), make_pair(CraftingSystem::getItemInfo(BANDAGE), Vector2D(4, 4)),
 			make_pair(CraftingSystem::getItemInfo(BANDAGE), Vector2D(4, 2))
@@ -184,11 +184,11 @@ void Game::initLoot() {
 		{
 			make_pair(CraftingSystem::getItemInfo(BANDAGE), Vector2D(0, 1)), make_pair(CraftingSystem::getItemInfo(WATER), Vector2D(0, 2)),
 			make_pair(CraftingSystem::getItemInfo(ELECTRONIC_REMAINS), Vector2D(2, 4)), make_pair(CraftingSystem::getItemInfo(FOOD), Vector2D(2, 0)),
-			make_pair(CraftingSystem::getItemInfo(CLASSIC_AMMO), Vector2D(4, 0)), make_pair(CraftingSystem::getItemInfo(BUILDING_PARTS), Vector2D(3, 2)),
+			make_pair(CraftingSystem::getItemInfo(CLASSIC_AMMO), Vector2D(4, 0)), make_pair(CraftingSystem::getItemInfo(SPACESHIP_KEY_ITEMS), Vector2D(3, 2)),
 			make_pair(CraftingSystem::getItemInfo(RICOCHET_AMMO), Vector2D(0, 4))
 		},
 		{ 
-			make_pair(CraftingSystem::getItemInfo(BUILDING_PARTS), Vector2D(3, 3)),
+			make_pair(CraftingSystem::getItemInfo(BUILDING_PARTS), Vector2D(3, 3)), make_pair(CraftingSystem::getItemInfo(SPACESHIP_KEY_ITEMS), Vector2D(0, 0)),
 			make_pair(CraftingSystem::getItemInfo(UPGRADE_KIT), Vector2D(2, 0)), make_pair(CraftingSystem::getItemInfo(BANDAGE), Vector2D(4, 0)), 
 			make_pair(CraftingSystem::getItemInfo(BANDAGE), Vector2D(1, 3)), make_pair(CraftingSystem::getItemInfo(BANDAGE), Vector2D(4, 2)),
 			make_pair(CraftingSystem::getItemInfo(MECANICAL_COMPONENTS), Vector2D(0, 4)), make_pair(CraftingSystem::getItemInfo(MEDICAL_COMPONENTS), Vector2D(2, 3))
