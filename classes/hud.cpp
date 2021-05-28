06f47204d8cf1f5cc17c7b3e0ef16360416ff4a8
iquintasALT <iquintas@ucm.es>
2021-05-28 02:40:59 +0200

    changed healthState descriptions

--- classes/hud.cpp
+++ classes/hud.cpp
@@ -48,12 +48,12 @@ hud::hud(Manager* m, Transform* initialPos, Player* p, Countdown* time_) : Entit
 	stateBackground->setAlpha(100);
 
 	woundDescriptions = std::vector<std::string>{
-		{"You are intoxicated, find something to detox"},
-		{"You suffered a contusion, now, you can't jump"},
-		{"You feel a lot of pain, find some painkiller"},
-		{"You are so cold, leave now!"},
-		{"You are bleeding"}, 
-		{"You have a serious wound"}
+		{"You are INTOXICATED, your are getting hungry..."},
+		{"You suffered a CONTUSSION, now, you can't jump..."},
+		{"You feel a lot of PAIN, you can`t aim well..."},
+		{"You are FREEZING, leave now!"},
+		{"You are BLEEDING"}, 
+		{"You have a serious wound..."}
 	};
 }
 
